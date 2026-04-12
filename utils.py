2407ee91e5f65565b933e64d7cec83931ee58cd8
MigCas31 <migcastells@gmail.com>
2026-04-12 18:12:36 +0200

    idea poster and registers

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import torch 
 
 def load_model(device="cpu"):
-    model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14').to(device) # Dinov2 ViT-B/14 (16 does not exist)
+    model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14_reg').to(device) # Dinov2 ViT-B/14 (16 does not exist)
     return model
