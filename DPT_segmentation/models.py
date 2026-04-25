ba1bd1f8ff2a6723b0f349ec6abb31ec8a0fe7c1
ThibautHeim <thibaut.heim@gmail.com>
2026-04-25 20:27:27 +0200

    integration of the segmentation pipeline

---
+++ DPT_segmentation/models.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from base_model import BaseModel
+from blocks import (
+    FeatureFusionBlock,
+    FeatureFusionBlock_custom,
+    Interpolate,
+    _make_encoder,
+    forward_vit,
+    readout_concatenate
+)
+
+
+def _make_fusion_block(features, use_bn):
+    return FeatureFusionBlock_custom(
+        features,
+        nn.ReLU(False),
+        deconv=False,
+        bn=use_bn,
+        expand=False,
+        align_corners=True,
+    )
+
+
+class DPT(BaseModel):
+    def __init__(
+        self,
+        head,
+        features=256,
+        backbone="vitb14_224",
+        readout="project",
+        channels_last=False,
+        use_bn=False,
+        enable_attention_hooks=False,
+    ):
+
+        super(DPT, self).__init__()
+
+        self.channels_last = channels_last
+
+        hooks = {
+            "vitb14_224": [2, 5, 8, 11] 
+        }
+
+        # Instantiate backbone and reassemble blocks
+        self.pretrained, self.scratch = _make_encoder(
+            backbone,
+            features,
+            False,  # Set to true of you want to train from scratch, uses ImageNet weights
+            groups=1,
+            expand=False,
+            exportable=False,
+            hooks=hooks[backbone],
+            use_readout=readout,
+            enable_attention_hooks=enable_attention_hooks,
+        )
+
+        self.scratch.refinenet1 = _make_fusion_block(features, use_bn)
+        self.scratch.refinenet2 = _make_fusion_block(features, use_bn)
+        self.scratch.refinenet3 = _make_fusion_block(features, use_bn)
+        self.scratch.refinenet4 = _make_fusion_block(features, use_bn)
+
+        self.scratch.output_conv = head
+
+    def forward(self, x, features = True):
+        if self.channels_last == True:
+            x.contiguous(memory_format=torch.channels_last)
+
+        if features :
+            layer_1, layer_2, layer_3, layer_4 = x # output layers of vit directly
+        else :
+            layer_1, layer_2, layer_3, layer_4 = forward_vit(self.pretrained, x) # Go through ViT 
+        layer_1, layer_2, layer_3, layer_4 = readout_concatenate(self.pretrained, layer_1, layer_2, layer_3, layer_4 ) # Readout and concatenate operations
+        # layer_1 : Bx96x64x64 -> Bx96x56x56,
+        # layer_2 : Bx192x32x32 -> Bx192x28x28,
+        # layer_3 : Bx384x16x16 -> Bx384x14x14,
+        # layer_4 : Bx768x8x8 -> Bx768x7x7
+
+        # Resample operations
+        layer_1_rn = self.scratch.layer1_rn(layer_1) # layer_1_rn : Bx96x64x64   
+        layer_2_rn = self.scratch.layer2_rn(layer_2) # layer_2_rn : Bx192x32x32
+        layer_3_rn = self.scratch.layer3_rn(layer_3) # layer_3_rn : Bx384x16x16
+        layer_4_rn = self.scratch.layer4_rn(layer_4) # layer_4_rn : Bx768x8x8
+
+        # Fusion operations
+        path_4 = self.scratch.refinenet4(layer_4_rn)          # path_4 : Bx256x16x16
+        path_3 = self.scratch.refinenet3(path_4, layer_3_rn)  # path_3 : Bx256x32x32
+        path_2 = self.scratch.refinenet2(path_3, layer_2_rn)  # path_2 : Bx256x64x64
+        path_1 = self.scratch.refinenet1(path_2, layer_1_rn)  # path_1 : Bx256x128x128 
+
+        # Final convolution 
+        out = self.scratch.output_conv(path_1)
+        
+        
+        return path_2, out # We return path_2 instead to be able to use the auxiliary head. 
+                            
+        
+
+
+
+
+
+class DPTSegmentationModel(DPT):
+    def __init__(self, num_classes = 150, path=None, **kwargs): 
+
+        features = kwargs["features"] if "features" in kwargs else 256
+
+        kwargs["use_bn"] = True
+
+        head = nn.Sequential(
+    
+            nn.Conv2d(features, features, kernel_size=3, padding=1, bias=False), 
+            nn.BatchNorm2d(features),
+            nn.ReLU(True),
+            nn.Dropout(0.1, False),
+            nn.Conv2d(features, num_classes, kernel_size=1),
+            nn.Upsample(size=(224, 224)) 
+        )
+
+        super().__init__(head, **kwargs) # Replaces the classic DPT head with a segmentation head
+
+        self.auxlayer = nn.Sequential(
+            nn.Conv2d(features, features, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(features),
+            nn.ReLU(True),
+            nn.Dropout(0.1, False),
+            nn.Conv2d(features, num_classes, kernel_size=1),
+            nn.Upsample(size=(224, 224)) 
+        )
+
+        if path is not None:
+            self.load(path)
